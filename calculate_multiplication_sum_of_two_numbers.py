3897bf7789f648b7f15b8cf56f5375306775a4cb
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-20 20:21:48 +0800

    Creating the codes for input numbers

--- calculate_multiplication_sum_of_two_numbers.py
+++ calculate_multiplication_sum_of_two_numbers.py
@@ -1,1 +1,13 @@
 # Calculate the multiplication and sum of two numbers
+
+def calculate_product_or_sum(input_1, input_2):
+    # calculate the two input numbers
+    product = input_1 * input_2
+    
+    # calculate the product that is less than 1000
+    if product <= 1000:
+        return product
+    
+    else:
+        return input_1 + input_2
+
